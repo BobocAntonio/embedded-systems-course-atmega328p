b54d4755c37fd2b2541b249268ecd91e88d25ebd
BOBOC_ ANTONIO <boboc.antonio.v2b@student.ucv.ro>
2026-03-11 20:17:37 +0200

    LAB1_EX2

--- src/main.c
+++ src/main.c
@@ -4,18 +4,21 @@
 #include "utils/delay.h"
 
 int main(void) {
-    //1. Blink configurabil: LED-ul clipeste la 3 frecvente configurate in cod.
+    //2.Counter pe LED-uri: foloseste 4 LED-uri ca afisare binara pentru un contor 0...15.
     Timer0_Init();
-    GPIO_Init(LED_BUILTIN, GPIO_OUTPUT);
-
-    int perioade[] = {1000,2000,3000}; // in ms
-    uint32_t last_value = 0;
-    uint8_t index = 0;
+    GPIO_Init(B10,GPIO_OUTPUT);
+    GPIO_Init(B11,GPIO_OUTPUT);
+    GPIO_Init(B12,GPIO_OUTPUT);
+    GPIO_Init(B13,GPIO_OUTPUT);
+    uint8_t i;
+ 
     while (1) {
-        if(Millis() - last_value >= perioade[index]) {
-            GPIO_Toggle(LED_BUILTIN);
-            last_value = Millis();
-            index = (index + 1) % 3;
+        for (i = 0; i < 16; i++) {
+            GPIO_WritePin(B10, (i >> 0) & 1);
+            GPIO_WritePin(B11, (i >> 1) & 1);
+            GPIO_WritePin(B12, (i >> 2) & 1);
+            GPIO_WritePin(B13, (i >> 3) & 1);
+            Delay_ms(1000);
         }
 
     }
